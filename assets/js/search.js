399946f0aaa8c5aa31671439e972dd7ef9d2dd15
JasonT <tzefungtsui@gmail.com>
2019-07-15 13:38:35 -0400

    search not complete, subscribe function missing

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,7 @@
-$("#search-field").ghostHunter({
-    results           : "#results",
-    info_template     : "<p>Number of posts found: {{amount}}</p>",
+var searchField=$("#search-field").ghostHunter({
+	results: "#results",
+	onKeyUp: true,
+	includebodysearch: true,
+	info_template: "<div class='row results_info'>Posts found: {{amount}}</div>",
+	result_template: "<a href='{{link}}' id='gh-{{ref}}' class='row result_item gh-search-item'><div class='column result_title'>{{title}}</div><div class='column result_date'>{{pubDate}}</div></a>"
 });
